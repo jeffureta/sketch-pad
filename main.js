5e403e20674a430f6aae100cd6ad722651e05d3a
jeffureta <jeffvincentureta@gmail.com>
2023-03-05 08:55:47 +0000

    Add basic UI and functions

--- main.js
+++ main.js
@@ -1,12 +1,61 @@
-const mainContainer = document.querySelector("#main-container");
+let color = "black";
+let click = true;
 
-for (let i = 0; i < 3; i++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
-    for(let j = 0; j < 3; j++) {
-        const square = document.createElement("div");
-        square.classList.add("square");
-        row.appendChild(square);
+function populateBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = document.querySelectorAll("div");
+    squares.forEach(div => div.remove);
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    
+    let amount = size * size; 
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement("div");
+        square.addEventListener("mouseover", colorSquare)
+        square.style.backgroundColor = "white";
+        board.insertAdjacentElement("beforeend", square)
+    }
 }
-mainContainer.appendChild(row);
+
+populateBoard(16);
+
+function changeSize(input) {
+    if (input >= 2 && input <= 100) {
+        document.querySelector(".error").style.display = "none";
+        populateBoard(input);
+    } else {
+        document.querySelector("error").style.display = "flex";
+    }
+     
 }
+
+function colorSquare() {
+    if (click) {
+        if (color === "random") {
+            this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+         } else {
+            this.style.backgroundColor = color;
+         }
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = document.querySelectorAll("div");
+    squares.forEach(div => div.style.backgroundColor = "white");
+}
+
+document.querySelector("body").addEventListener("click", (e) => {
+    if (e.target.tagName != "BUTTON") {
+        click = !click;
+    if(click) {
+        document.querySelector(".mode").textContent = "mode: coloring";
+    } else {
+        document.querySelector(".mode").textContent = "mode: not coloring";
+    }
+    }
+})
